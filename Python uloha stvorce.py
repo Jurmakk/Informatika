be26bd65c3658bbbbaebd6f94a1484f958a5a379
Jurmakk <56343671+Jurmakk@users.noreply.github.com>
2020-01-07 11:09:30 +0100

    Add files via upload

---
+++ Python uloha stvorce.py
@@ -0,0 +1,33 @@
+from tkinter import *
+import random
+
+
+master= Tk()
+canvas_width = 800
+canvas_height = 600
+
+w = Canvas(master, width = canvas_width, height = canvas_height)
+w.pack()
+
+def stvorec(x,y,width, height):
+    w.create_rectangle(x-width/2,y+height/2, x+width/2, y-height/2)
+
+def popici_stvorec(x,y,width,height, space, num):
+    if(x-width/2>0 and x+width/2<canvas_width and y-height/2>0 and y+height/2<canvas_height):
+        for i in range(num):
+            stvorec(x,y,width-i*space*2, height-i*space*2)
+        return True
+    else:
+        return False
+x =0
+
+while(x<=10):
+    n = random.randint(5,10)
+    xko = random.randint(0,800)
+    yko = random.randint(0,600)
+    h = random.randint(150,300)
+    j = random.randint(150,300)
+    medz  =random.randint(5,10)
+    if(popici_stvorec(xko,yko,h,j,medz,n)==True):
+        x+=1
+    
